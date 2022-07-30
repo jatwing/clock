bc1f76f4771fb40746af3b5656dbfd45ebffcd49
jatwing <jatwing@pm.me>
2022-07-31 00:08:58 +0800

    js load current time and display.

--- scripts/clock.js
+++ scripts/clock.js
@@ -1,1 +1,36 @@
-console.log('hello');
+console.log("hello");
+
+const setDigit = (flapDisplayId, value) => {
+  const flapBackTop = document.querySelector(
+    `#${flapDisplayId} .flap.back.top`
+  );
+  const flapFrontTop = document.querySelector(
+    `#${flapDisplayId} .flap.front.top`
+  );
+  const flapFrontBottom = document.querySelector(
+    `#${flapDisplayId} .flap.front.bottom`
+  );
+  const flapBackBottom = document.querySelector(
+    `#${flapDisplayId} .flap.back.bottom`
+  );
+  flapBackTop.textContent = value;
+  flapFrontTop.textContent = value;
+  flapFrontBottom.textContent = value;
+  flapBackBottom.textContent = value;
+};
+
+const showTime = () => {
+  const now = new Date();
+  const hour = now.getHours();
+  const minute = now.getMinutes();
+  const second = now.getSeconds();
+  // TODO am pm
+  setDigit("hour-first-digit", Math.floor(hour / 10));
+  setDigit("hour-second-digit", hour % 10);
+  setDigit("minute-first-digit", Math.floor(minute / 10));
+  setDigit("minute-second-digit", minute % 10);
+  setDigit("second-first-digit", Math.floor(second / 10));
+  setDigit("second-second-digit", second % 10);
+};
+
+const interval = setInterval(showTime, 1000);
