35a2f14c6464bf6bd8c073e5c50db8a4ff845031
jatwing <jatwing@proton.me>
2022-09-11 18:01:27 +0800

    rename body block as page. fullscreen support multi browsers. clip-path of flap use polygon to support chrome.

--- scripts/header.js
+++ scripts/header.js
@@ -1,39 +1,59 @@
-const header = document.querySelector(".header");
-const palette = document.querySelector('#palette');
-const fullscreen = document.querySelector("#fullscreen");
-const clock = document.querySelector(".clock");
-
 const changeColor = () => {
-  if (header.classList.contains('header--red')) {
-    header.classList.remove('header--red');
-    clock.classList.remove('clock--red');
+  const header = document.querySelector(".header");
+  const clock = document.querySelector(".clock");
+  if (header.classList.contains("header--red")) {
+    header.classList.remove("header--red");
+    clock.classList.remove("clock--red");
     return;
-  } 
-  header.classList.add('header--red');
-  clock.classList.add('clock--red');
+  }
+  header.classList.add("header--red");
+  clock.classList.add("clock--red");
 };
 
-const demandFullscreen = () => {
-  if (document.fullscreenEnabled) {
-    clock.requestFullscreen();
+const demandFullscreen = (element) => {
+  if (
+    document.fullscreenElement ||
+    document.mozFullScreenElement ||
+    document.msFullscreenElement ||
+    document.webkitFullscreenElement
+  ) {
+    return;
   }
-  header.classList.add("header--hidden");
+  if (element.requestFullscreen) {
+    element.requestFullscreen();
+  } else if (element.mozRequestFullScreen) {
+    element.mozRequestFullScreen();
+  } else if (element.msRequestFullscreen) {
+    element.msRequestFullscreen();
+  } else if (element.webkitRequestFullscreen) {
+    element.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+  }
 };
 
 const quitFullscreen = () => {
-  if (document.fullscreenEnabled && document.fullscreenElement) {
+  if (
+    !document.fullscreenElement &&
+    !document.mozFullScreenElement &&
+    !document.msFullscreenElement &&
+    !document.webkitFullscreenElement
+  ) {
+    return;
+  }
+  if (document.exitFullscreen) {
     document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  } else if (document.msExitFullscreen) {
+    document.msExitFullscreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
   }
-  header.classList.remove("header--hidden");
 };
 
-const changeFullscreen = () => {
-  if (!document.fullscreenElement) {
-    header.classList.remove("header--hidden");
-  }
-};
+const palette = document.querySelector("#palette");
+const fullscreen = document.querySelector("#fullscreen");
+const clock = document.querySelector(".clock");
 
-palette.addEventListener("click", changeColor, false);
-fullscreen.addEventListener("click", demandFullscreen, false);
-clock.addEventListener("click", quitFullscreen, false);
-clock.addEventListener("fullscreenchange", changeFullscreen);
+palette.addEventListener("click", () => changeColor());
+fullscreen.addEventListener("click", () => demandFullscreen(clock));
+clock.addEventListener("click", () => quitFullscreen());
