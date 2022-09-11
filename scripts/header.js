6f780c96a07ad3afbb05614bf2852d665167c175
jatwing <jatwing@proton.me>
2022-09-11 13:07:47 +0800

    add palette button to switch color.

--- scripts/header.js
+++ scripts/header.js
@@ -1,6 +1,17 @@
 const header = document.querySelector(".header");
+const palette = document.querySelector('#palette');
 const fullscreen = document.querySelector("#fullscreen");
 const clock = document.querySelector(".clock");
+
+const changeColor = () => {
+  if (header.classList.contains('header--red')) {
+    header.classList.remove('header--red');
+    clock.classList.remove('clock--red');
+    return;
+  } 
+  header.classList.add('header--red');
+  clock.classList.add('clock--red');
+};
 
 const demandFullscreen = () => {
   if (document.fullscreenEnabled) {
@@ -22,6 +33,7 @@ const changeFullscreen = () => {
   }
 };
 
+palette.addEventListener("click", changeColor, false);
 fullscreen.addEventListener("click", demandFullscreen, false);
 clock.addEventListener("click", quitFullscreen, false);
 clock.addEventListener("fullscreenchange", changeFullscreen);
